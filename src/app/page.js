fd89ae5d29cbbec730fd31db1fe1b4808d4eb57b
iamrivu6 <115791425+iamrivu6@users.noreply.github.com>
2023-08-23 14:10:54 +0530

    chabges

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
   const [lastPage, setLastPage] = useState(false);
   const [buyerListing, setBuyerListing] = useState([
     {
-      Product: "Circuit boards",
+      Product: "Circuit Boards",
       Seller: "MV Enterprise",
       Contact: "+917822671187",
     },
